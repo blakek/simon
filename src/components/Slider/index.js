d2be83bfacaa9f5889f002252c3dcd596d984161
Blake Knight <oss.ideas@gmail.com>
2017-01-24 21:44:23 -0600

    🎨 range inputs have custom blue styles

--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -1,7 +1,47 @@
 import React, { PropTypes } from 'react'
+import styled from 'styled-components'
+
+const Input = styled.input`
+  appearance: none;
+  height: 1rem;
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+  opacity: .8;
+  background-color: transparent;
+
+  &:focus {
+    outline: none;
+  }
+
+  &:hover {
+    cursor: pointer;
+  }
+
+  &::-webkit-slider-runnable-track {
+    height: .2rem;
+    background-color: skyblue;
+    cursor: pointer;
+    border-radius: 1.3px;
+  }
+
+  &::-webkit-slider-thumb {
+    appearance: none;
+    background-color: skyblue;
+    opacity: .7;
+    height: 1rem;
+    width: 1rem;
+    border-radius: 50%;
+    margin-top: -.4rem;
+
+    &:hover {
+      opacity: .85;
+      transform: scale(1.3);
+    }
+  }
+`
 
 export const Slider = ({ max, min, onChange, step, value }) => (
-  <input
+  <Input
     max={max}
     min={min}
     step={step}
