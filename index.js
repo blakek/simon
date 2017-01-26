b8ecfd86389b47b1590d7e0801ba4b2d48f61df1
Blake Knight <oss.ideas@gmail.com>
2017-01-25 22:47:28 -0600

    :art: set default window background for Electron and browser

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const path = require('path')
 const Menubar = require('menubar')
 
 const menubar = Menubar({
-  dir: path.join(__dirname)
+  dir: path.join(__dirname),
+  backgroundColor: '#0001'
 })
 
 menubar.on('after-create-window', () => {
