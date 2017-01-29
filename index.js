60342b911b8c9757556680cc0c535011df6f9303
Blake Knight <oss.ideas@gmail.com>
2017-01-29 15:37:31 -0600

    ↔️ adjust window sizing

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const Menubar = require('menubar')
 
 const menubar = Menubar({
   dir: path.join(__dirname),
-  backgroundColor: '#0001'
+  backgroundColor: '#0001',
+  height: 250,
+  width: 425
 })
 
 menubar.on('after-create-window', () => {
