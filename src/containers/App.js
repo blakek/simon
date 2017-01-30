81c203508595679c16b323465392853980bf71c7
Blake Knight <oss.ideas@gmail.com>
2017-01-29 20:42:09 -0600

    :wrench: separate logic into different "screen" components

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,18 +1,7 @@
 import React, { Component } from 'react'
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from 'lib/themes'
-import { Button } from 'components/Button'
-import { GroupNameInput } from 'components/GroupNameInput'
-import { SpeechPitchSlider } from 'components/SpeechPitchSlider'
-import { SpeechRateSlider } from 'components/SpeechRateSlider'
-import { TextArea } from 'components/TextArea'
-import { VoiceList } from 'components/VoiceList'
-
-const containerStyle = {
-  display: 'flex',
-  flex: 1,
-  flexFlow: 'column wrap'
-}
+import { Send } from 'screens/Send'
 
 export class App extends Component {
   constructor() {
@@ -20,8 +9,8 @@ export class App extends Component {
 
     this.state = {
       group: '',
-      pitch: 1,
-      rate: 1,
+      speechPitch: 1,
+      speechRate: 1,
       text: '',
       voice: null,
       voices: []
@@ -36,8 +25,8 @@ export class App extends Component {
 
   speakText() {
     const utterance = new SpeechSynthesisUtterance(this.state.text)
-    utterance.pitch = this.state.pitch
-    utterance.rate = this.state.rate
+    utterance.pitch = this.state.speechPitch
+    utterance.rate = this.state.speechRate
     utterance.voice = this.state.voice
 
     speechSynthesis.speak(utterance)
@@ -46,36 +35,15 @@ export class App extends Component {
   render() {
     return (
       <ThemeProvider theme={defaultTheme}>
-        <div style={containerStyle}>
-          <TextArea
-            maxRows={5}
-            placeholder="Say something&hellip;"
-            value={this.state.text}
-            onChange={text => this.setState({ text })}
-          />
-
-          <VoiceList
-            voices={this.state.voices}
-            onChange={voice => this.setState({ voice })}
-          />
-
-          <SpeechRateSlider
-            value={this.state.rate}
-            onChange={rate => this.setState({ rate })}
-          />
-
-          <SpeechPitchSlider
-            value={this.state.pitch}
-            onChange={pitch => this.setState({ pitch })}
-          />
-
-          <GroupNameInput
-            onChange={group => this.setState({ group })}
-            value={this.state.group}
-          />
-
-          <Button onClick={() => this.speakText()}>Test</Button>
-        </div>
+        <Send
+          onTextChanged={text => this.setState({ text })}
+          onVoiceChanged={voice => this.setState({ voice })}
+          onSpeechRateChanged={speechRate => this.setState({ speechRate })}
+          onSpeechPitchChanged={speechPitch => this.setState({ speechPitch })}
+          onGroupChanged={group => this.setState({ group })}
+          testSpeech={() => this.speakText()}
+          {...this.state}
+        />
       </ThemeProvider>
     )
   }
---
+++ src/screens/Send.js
@@ -0,0 +1,75 @@
+import React, { PropTypes } from 'react'
+import styled from 'styled-components'
+import { Button } from 'components/Button'
+import { GroupNameInput } from 'components/GroupNameInput'
+import { SpeechPitchSlider } from 'components/SpeechPitchSlider'
+import { SpeechRateSlider } from 'components/SpeechRateSlider'
+import { TextArea } from 'components/TextArea'
+import { VoiceList } from 'components/VoiceList'
+
+const AppContainer = styled.section`
+  display: flex;
+  flex: 1;
+  flex-flow: column wrap;
+`
+
+export const Send = ({
+  text,
+  onTextChanged,
+  voices,
+  onVoiceChanged,
+  speechRate,
+  onSpeechRateChanged,
+  speechPitch,
+  onSpeechPitchChanged,
+  group,
+  onGroupChanged,
+  testSpeech
+}) => (
+  <AppContainer>
+    <TextArea
+      maxRows={5}
+      placeholder="Say something&hellip;"
+      value={text}
+      onChange={onTextChanged}
+    />
+
+    <VoiceList
+      voices={voices}
+      onChange={onVoiceChanged}
+    />
+
+    <SpeechRateSlider
+      value={speechRate}
+      onChange={onSpeechRateChanged}
+    />
+
+    <SpeechPitchSlider
+      value={speechPitch}
+      onChange={onSpeechPitchChanged}
+    />
+
+    <GroupNameInput
+      value={group}
+      onChange={onGroupChanged}
+    />
+
+    <Button onClick={testSpeech}>Test</Button>
+  </AppContainer>
+)
+
+Send.propTypes = {
+  text: PropTypes.string.isRequired,
+  onTextChanged: PropTypes.func.isRequired,
+  voices: PropTypes.array.isRequired,
+  onVoiceChanged: PropTypes.func.isRequired,
+  speechRate: PropTypes.number.isRequired,
+  onSpeechRateChanged: PropTypes.func.isRequired,
+  speechPitch: PropTypes.number.isRequired,
+  onSpeechPitchChanged: PropTypes.func.isRequired,
+  group: PropTypes.string.isRequired,
+  onGroupChanged: PropTypes.func.isRequired,
+  testSpeech: PropTypes.func.isRequired
+}
+
+export default Send
