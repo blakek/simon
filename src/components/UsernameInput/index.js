c7af0e87d771c261179a02694f28fac8c2545cf9
Blake Knight <oss.ideas@gmail.com>
2017-01-30 21:12:31 -0600

    :sparkles: make username component more reusable

--- src/components/UsernameInput/index.js
+++ src/components/UsernameInput/index.js
@@ -1,15 +1,20 @@
 import React, { PropTypes } from 'react'
 import { TextInput } from 'components/TextInput'
 
-export const UsernameInput = ({ onChange, value }) => (
+export const UsernameInput = ({ onChange, title, value }) => (
   <TextInput
     onChange={onChange}
-    title="Username"
+    title={title}
     value={value}
   />
 )
 
 UsernameInput.propTypes = {
   onChange: PropTypes.func.isRequired,
+  title: PropTypes.string.isRequired,
   value: PropTypes.string
+}
+
+UsernameInput.defaultProps = {
+  title: 'Username'
 }
