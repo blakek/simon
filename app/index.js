00c6a47af8b1426592ec598f7ff847acd4b9eaad
Blake Knight <oss.ideas@gmail.com>
2017-02-10 06:13:09 -0600

    🚧 adding new build process

--- app/index.js
+++ app/index.js
@@ -2,7 +2,8 @@ const path = require('path')
 const Menubar = require('menubar')
 
 const menubar = Menubar({
-  dir: path.join(__dirname),
+  dir: __dirname,
+  icon: path.join(__dirname, 'resources/TrayIcon.png'),
   backgroundColor: '#0001',
   height: 250,
   width: 425
